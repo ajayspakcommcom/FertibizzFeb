f64f9fd41b00018575c744ed319db45e2ae334ec
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2023-01-03 14:01:12 +0530

    latest changes

--- public/js/potential.js
+++ public/js/potential.js
@@ -1,4 +1,16 @@
 
+function loadMonthYear() {
+    const date = new Date();
+    let dt = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+    $('#cmbMonth').val(dt.getMonth() + 1); // our combo box starts with 1
+    $('#cmbYear').val(dt.getFullYear());
+    $('#cmbMonth').prop('disabled', true);
+    $('#cmbYear').prop('disabled', true);
+  }
+
+  loadMonthYear();
+  
+
 
 function validateMe() {
 
