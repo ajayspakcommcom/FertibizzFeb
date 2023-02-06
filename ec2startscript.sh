2fd5da19d077240a753c0bfbb7eeb9df810df62d
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2023-02-06 18:06:47 +0530

    latest changes

---
+++ ec2startscript.sh
@@ -0,0 +1,13 @@
+  {
+	"StartInstances" : [
+		"InstanceId": "dfdasf",
+		"CurrentState": {
+			"Code":0,
+			"Name":"pending"
+		},
+		"PreviousState": {
+			"Code": 80,
+			"Name": "stopped"
+		}
+	]
+  }
