dd4f0b8be754457fc7f84c85c08ac70219d0a065
Ajay Mahavir Vishwakarma <46556928+ajayspak@users.noreply.github.com>
2023-03-06 00:50:04 +0530

    latest-changes

--- public/js/common.js
+++ public/js/common.js
@@ -191,7 +191,7 @@ function getIdFromURL() {
 
 function getFirstDayPreviousMonth() {
     const date = new Date();
-    let dt = new Date(date.getFullYear(), date.getMonth() - 2, 1);
+    let dt = new Date(date.getFullYear(), date.getMonth() - 1, 1);
     $('#cmbMonth').val(dt.getMonth() + 1); // our combo box starts with 1
     $('#cmbYear').val(dt.getFullYear());
     //$('#cmbMonth').attr('disabled', 'disabled');
