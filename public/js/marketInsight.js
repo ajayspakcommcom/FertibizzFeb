3abdfd751abeedd27ffffd6471ff4d84b3cc861d
Ajay Mahavir Vishwakarma <46556928+ajayspak@users.noreply.github.com>
2023-03-06 00:50:22 +0530

    lc

--- public/js/marketInsight.js
+++ public/js/marketInsight.js
@@ -2,7 +2,7 @@
 
 function loadMonthYear() {
     const date = new Date();
-    let dt = new Date(date.getFullYear(), date.getMonth() - 2, 1);
+    let dt = new Date(date.getFullYear(), date.getMonth() - 1, 1);
     $('#cmbMonth').val(dt.getMonth() + 1); // our combo box starts with 1
     $('#cmbYear').val(dt.getFullYear());
     $('#cmbMonth').prop('disabled', true);
